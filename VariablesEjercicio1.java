0f399593f4bd609eb6acab283f0753e10c484a4d
anamart <100534348+MtzAna26@users.noreply.github.com>
2025-01-29 13:35:30 -0600

    Ejercicios de varibles 1  y 2

---
+++ VariablesEjercicio1.java
@@ -0,0 +1,18 @@
+// Declare e inicie variables que permita realizar las operaciones básicas.
+class VariablesEjercicio1{
+    public static void main (String[] args){
+        int a = 5, b = 3, c;
+        System.out.println("La suma es: ");
+        c=a+b;
+        System.out.println(c);
+        System.out.println("La resta es: ");
+        c=a-b;
+        System.out.println(c);
+        System.out.println("La multiplicación es: ");
+        c=a*b;
+        System.out.println(c);
+        System.out.println("La división es: ");
+        double d=a/b;
+        System.out.println(d);
+    }
+}
---
+++ VariablesEjercicio2.java
@@ -0,0 +1,20 @@
+// Mostrar el promedio de notas de un alumno
+class VariablesEjercicio2{
+    public static void main (String[] args){
+    System.out.println("Promedio de notas");
+    System.out.println("nota 01");
+    int n1= 16;
+    System.out.println(n1);
+    System.out.println("nota 02");
+    int n2= 17;
+    System.out.println(n2);
+    System.out.println("nota 03");
+    int n3= 11;
+    System.out.println(n3);
+    System.out.println("nota 04");
+    int n4= 14;
+    System.out.println(n4);
+    float promedio =(n1+n2+n3+n4)/4;
+    System.out.println("Su promedio es ; " + promedio); 
+    }
+}
