0ab93c99c287eb889ddb1e48de617f7d78312d80
anamart <100534348+MtzAna26@users.noreply.github.com>
2025-01-29 12:28:07 -0600

    Primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+class HolaMundo {
+    /*public class HolaMundo{
+        public static void main (String[] args){
+            System.out.println("Hola Mundo, no se que estoy haciendo no le entiendo al tutor");
+        }
+    }*/
+   public static void main (String[] args){
+            System.out.println("Hola Mundo, no se que estoy haciendo no le entiendo al tutor");
+        }
+}
---
+++ Variables.java
@@ -0,0 +1,12 @@
+class Variables{
+    public static void main (String[] args){
+        int version;
+        version = 23;
+    //System.out.println("Hola" + version);
+    System.out.println(version);
+
+    int versionJava;
+		versionJava = 15;
+		System.out.println(versionJava);
+    }
+}
